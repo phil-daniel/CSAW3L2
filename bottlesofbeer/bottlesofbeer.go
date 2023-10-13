30f1324c30487a2f95f0a611981d5181e5d36b61
Phillip Daniel <fv22283@bristol.ac.uk>
2023-10-13 15:38:51 +0100

    Completed secretstrings task

--- bottlesofbeer/bottlesofbeer.go
+++ bottlesofbeer/bottlesofbeer.go
@@ -1,20 +1,36 @@
 package main
+
 import (
 	"flag"
-//	"net/rpc"
-//	"fmt"
-//	"time"
-//	"net"
+	"fmt"
+	"net"
+	"net/rpc"
+	//	"net/rpc"
+	//	"fmt"
+	//	"time"
+	//	"net"
 )
 
 var nextAddr string
 
-
-func main(){
-//	thisPort := flag.String("this", "8030", "Port for this process to listen on")
+func main() {
+	thisPort := flag.String("this", "8030", "Port for this process to listen on")
 	flag.StringVar(&nextAddr, "next", "localhost:8040", "IP:Port string for next member of the round.")
-//	bottles := flag.Int("n",0, "Bottles of Beer (launches song if not 0)")
+	bottles := flag.Int("n", 0, "Bottles of Beer (launches song if not 0)")
 	flag.Parse()
 	//TODO: Up to you from here! Remember, you'll need to both listen for
 	//RPC calls and make your own.
+
+	listener, _ := net.Listen("tcp", ":"+*thisPort)
+	defer listener.Close()
+	rpc.Accept(listener)
+
+	if *bottles != 0 {
+		fmt.Printf("%d bottles of beer on the wall, %d bottles of beer. Take one down, pass it around...\n", *bottles, *bottles)
+		client, _ := rpc.Dial("tcp", nextAddr)
+		defer client.Close()
+
+		client.Go()
+		client.Call()
+	}
 }
--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,31 @@
 package main
+
 import (
-//	"net/rpc"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	"net/rpc"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+
+	//	"bufio"
+	//	"os"
 	"fmt"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
 	//TODO: connect to the RPC server and send the request(s)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	request := stubs.Request{Message: "Hello"}
+	response := new(stubs.Response)
+
+	client.Call(stubs.ReverseHandler, request, response)
+	fmt.Println("Responded: " + response.Message)
+
+	request1 := stubs.Request{Message: "Hello"}
+	response1 := new(stubs.Response)
+	client.Call(stubs.PremiumReverseHandler, request1, response1)
+	fmt.Println("Fast response: " + response1.Message)
 }
